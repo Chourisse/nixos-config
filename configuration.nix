4951aff53ee948721473c5aa9f8015a8278d54ad
Chourisse <teamourspolaire@gmail.com>
2026-07-20 20:12:18 +0200

    Mise à jour config

--- configuration.nix
+++ configuration.nix
@@ -91,6 +91,15 @@
 
   programs.steam.enable = true;
 
+  # Auto cleanup
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+
+  nix.settings.auto-optimise-store = true;
+
   system.stateVersion = "26.05";
 
 }
