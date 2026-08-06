829b2cc2ac994c6f7d862d83132444bf5826311b
Chourisse <teamourspolaire@gmail.com>
2026-08-07 01:55:02 +0200

    Update Config

--- flake.nix
+++ flake.nix
@@ -1,11 +1,11 @@
 {
   description = "Configuration NixOS";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
